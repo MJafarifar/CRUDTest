f60ea4a50ba42b74a65e8349fae1903c62280f64
mohammadjafarifar <mohammadjafarifar@MOHAMMAD>
2020-12-27 19:49:03 +0330

    Add project files.

---
+++ CRUDTest/Migrations/20201227160839_init2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUDTest.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "TestStates",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "TestStates");
+        }
+    }
+}
---
+++ CRUDTest/Program.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace CRUDTest
+{
+    public class TestState
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Child
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+    public class Parent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Child> Children { get; set; }
+    }
+
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer($"Data Source=.;Initial Catalog=CourseDb;Integrated Security=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<TestState>().Property<bool>("isDeleted");
+            builder.Entity<TestState>().HasQueryFilter(m => EF.Property<bool>(m, "isDeleted") == false);
+        }
+        public DbSet<TestState> TestStates { get; set; }
+        public override int SaveChanges()
+        {
+            UpdateSoftDeleteStatuses();
+            return base.SaveChanges();
+        }
+
+        
+
+        private void UpdateSoftDeleteStatuses()
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.CurrentValues["isDeleted"] = false;
+                        break;
+                    case EntityState.Deleted:
+                        entry.State = EntityState.Modified;
+                        entry.CurrentValues["isDeleted"] = true;
+                        break;
+                }
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckState();
+            MyDbContext ctx = new MyDbContext();
+            var state=ctx.Find<TestState>(2);
+            ctx.Remove(state);
+            ctx.SaveChanges();
+            //var parent = new Parent
+            //{
+            //    Name = "Parnet",
+            //    Children = new List<Child> {
+            //        new Child
+            //        {
+            //            Name="Child01"
+            //        }
+
+            //    }
+            //};
+        }
+
+        private static void CheckState()
+        {
+            TestState testState = new TestState
+            {
+                Name = "نام دوم"
+            };
+            MyDbContext ctx = new MyDbContext();
+            Console.WriteLine(ctx.Entry(testState).State);
+            ctx.Add(testState);
+            Console.WriteLine(ctx.Entry(testState).State);
+            ctx.SaveChanges();
+            Console.WriteLine(ctx.Entry(testState).State);
+        }
+    }
+}
